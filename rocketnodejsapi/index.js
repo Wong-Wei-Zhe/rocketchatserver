5028183cb74f808bcfacbf39a47c77dd0dc21afd
Wong Wei Zhe Besquare <wongwz2828@gmail.com>
2022-03-16 08:30:38 +0000

    AWS Cloud formation Files

--- rocketnodejsapi/index.js
+++ rocketnodejsapi/index.js
@@ -5,7 +5,6 @@ var bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 var axios = require("axios");
 const { response } = require("express");
-const e = require("express");
 var MongoDBStore = require("connect-mongodb-session")(session);
 var store = new MongoDBStore({
   uri: "mongodb://localhost:27017/connect_mongodb_session_test",
@@ -25,8 +24,8 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const rocketAdmToken = "-C3I2nnJE6tVVLJdTohx71NpCiRQCaZjto-uK2KbDcD";
-const rocketAdmId = "KitTbPCQQaRM9onYX";
+const rocketAdmToken = "4rxe25g2uQ-o3VJeAW5hZlDhS26K2XNYSHWXm0RuZ-n";
+const rocketAdmId = "QTsScxbo2sEnSmLmS";
 const ROCKETCHAT_SERVER = "http://localhost:3005/";
 const ROCKETCHAT_API = `${ROCKETCHAT_SERVER}api/v1/`;
 const axiosConfig = {
@@ -56,16 +55,23 @@ app.use((req, res, next) => {
 });
 
 app.post("/getuser", function (req, res) {
-  axios
-    .get(`${ROCKETCHAT_API}users.info?username=new-user`, axiosConfig)
-    .then(function (response) {
-      if (response.data.success === true) {
-        res.send({ user: response.data.user, search_status: "success" });
-      }
-    })
-    .catch(function (error) {
-      res.status(401).send(error.response.data.error);
-    });
+  if (req.body.username) {
+    axios
+      .get(
+        `${ROCKETCHAT_API}users.info?username=${req.body.username}`,
+        axiosConfig
+      )
+      .then(function (response) {
+        if (response.data.success === true) {
+          res.send({ user: response.data.user, search_status: "success" });
+        }
+      })
+      .catch(function (error) {
+        res.status(404).send(error.response.data.error);
+      });
+  } else {
+    res.status(400).send("Bad Request");
+  }
 });
 
 app.post("/rocket_sso", function (req, res) {
@@ -170,6 +176,14 @@ app.post("/rocket_iframe", function (req, res) {
 					loginToken: '${req.session.userauthtoken}'
 				}, ${ROCKETCHAT_SERVER}); // rocket.chat's URL
 				</script>`);
+});
+
+app.post("/rocket_check_channel", function (req, res) {
+  console.log("WIP");
+});
+
+app.post("/rocket_create_channel", function (req, res) {
+  console.log("WIP");
 });
 
 app.get("/testapi", function (req, res) {
